f61fa3c3dc493d13c3153ba015b6bff0017ed377
Ashley Yakeley <ashley@semantic.org>
2007-12-08 16:52:05 -0800

    clean up
    darcs-hash:20071209005205-ac6dd-cd0cb6f3dc64ae3bcc45b3cf743ee1dd7e3aabb6

--- Data/Witness/Representative.hs
+++ Data/Witness/Representative.hs
@@ -3,25 +3,25 @@ module Data.Witness.Representative where
 	import Data.Witness.Any;
 
 	-- every type (in Is) has a representative
-	class Representative w where
+	class Representative rep where
 	{
-		withRepresentative :: forall r. (forall a. (Is w a) => w a -> r) -> (forall b. w b -> r);
+		withRepresentative :: forall r. (forall a. (Is rep a) => rep a -> r) -> (forall b. rep b -> r);
 	};
 
-	class (Representative w) => Is w a where
+	class (Representative rep) => Is rep a where
 	{
-		representative :: w a;
+		representative :: rep a;
 	};
 
-	getRepresentative :: (Is w a) => a -> w a;
+	getRepresentative :: (Is rep a) => a -> rep a;
 	getRepresentative _ = representative;
 
-	rerepresentative :: (Is w a) => p a -> w a;
+	rerepresentative :: (Is rep a) => p a -> rep a;
 	rerepresentative _ = representative;
 
-	mkAny :: (Is w a) => a -> Any w;
+	mkAny :: (Is rep a) => a -> Any rep;
 	mkAny a = MkAny representative a;
 
-	mkAnyF :: (Is w a) => f a -> AnyF w f;
+	mkAnyF :: (Is rep a) => f a -> AnyF rep f;
 	mkAnyF fa = MkAnyF representative fa;
 }
