d6cab77f253a5ca3b8cf8a34c73a798005b0131f
Ashley Yakeley <ashley@semantic.org>
2008-05-09 02:32:21 -0700

    work around haddock-2.0.0.0 bug
    darcs-hash:20080509093221-ac6dd-71f0b92ccfd80996642e7344bf2f8eef0321c25e

--- Data/Witness/Representative.hs
+++ Data/Witness/Representative.hs
@@ -8,12 +8,14 @@ module Data.Witness.Representative where
 	};
 
 	-- | Every value is an instance of Is
+	;
 	class Eq1 rep => Representative rep where
 	{
 		withRepresentative :: forall r. (forall a. (Is rep a) => rep a -> r) -> (forall b. rep b -> r);
 	};
 
 	-- | If two representatives have the same type, then they have the same value.
+	;
 	class Representative rep => Is rep a where
 	{
 		representative :: rep a;
--- Data/Witness/Witness.hs
+++ Data/Witness/Witness.hs
@@ -3,6 +3,7 @@ module Data.Witness.Witness where
 	import Data.Witness.SameType;
 
 	-- | If two witnesses have the same value, then they have the same type.
+	;
 	class Witness w where
 	{
 		matchWitness :: w a -> w a' -> Maybe (SameType a a');
