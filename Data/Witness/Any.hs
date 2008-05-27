982d2306c8492167e2c165d607efdf8533a075e6
Ashley Yakeley <ashley@semantic.org>
2008-05-26 17:51:31 -0700

    clean up WitnessDict
    darcs-hash:20080527005131-ac6dd-6154b326824ab2ad04b27754949549ac682ae86d

--- Data/Witness/Any.hs
+++ Data/Witness/Any.hs
@@ -1,8 +1,29 @@
 module Data.Witness.Any where
 {
+	import Data.Witness.Witness;
+	import Data.Witness.EqualType;
+	import Data.Maybe;
+
 	data Any w = forall a. MkAny (w a) a;
+	
+	matchAny :: (Witness w) => w a -> Any w -> Maybe a;
+	matchAny wit (MkAny cwit ca) = do
+	{
+		MkEqualType <- matchWitness cwit wit;
+		return ca;
+	};
 
 	data AnyF w f = forall a. MkAnyF (w a) (f a);
+	
+	matchAnyF :: (Witness w) => w a -> AnyF w f -> Maybe (f a);
+	matchAnyF wit (MkAnyF cwit cfa) = do
+	{
+		MkEqualType <- matchWitness cwit wit;
+		return cfa;
+	};
 
 	data AnyWitness w = forall a. MkAnyWitness (w a);
+	
+	matchAnyWitness :: (Witness w) => w a -> AnyWitness w -> Bool;
+	matchAnyWitness wit (MkAnyWitness cwit) = isJust (matchWitness cwit wit);
 }
--- Data/Witness/WitnessDict.hs
+++ Data/Witness/WitnessDict.hs
@@ -1,11 +1,8 @@
-module Data.Witness.WitnessDict
-(
-	WitnessDict,emptyWitnessDict,
-	witnessDictLookup,witnessDictReplace,witnessDictModify,witnessDictAdd,witnessDictFromList
-) where
+module Data.Witness.WitnessDict where
 {
 	import Data.Witness;
 	import Control.Monad.State;
+	import Data.Maybe;
 	
 	newtype WitnessDict w = MkWitnessDict [Any w];
 	
@@ -13,23 +10,11 @@ module Data.Witness.WitnessDict
 	emptyWitnessDict = MkWitnessDict[];
 	
 	witnessDictLookup :: (Witness w) => w a -> WitnessDict w -> Maybe a;
-	witnessDictLookup wit (MkWitnessDict cells) = findM (\(MkAny cwit ca) -> do
-	{
-		MkEqualType <- matchWitness cwit wit;
-		return ca;
-	}) cells where
-	{
-		findM :: (a -> Maybe b) -> [a] -> Maybe b;
-		findM match (a:as) = case match a of
-		{
-			mb@(Just _) -> mb;
-			_ -> findM match as;
-		};
-		findM _ _ = Nothing;
-	};
+	witnessDictLookup wit (MkWitnessDict cells) = listToMaybe (mapMaybe (matchAny wit) cells);
 	
 	witnessDictModify :: (Witness w) => w a -> (a -> a) -> WitnessDict w -> Maybe (WitnessDict w);
-	witnessDictModify wit amap (MkWitnessDict cc) = fmap MkWitnessDict (replaceFirst (mapCell wit amap) cc) where
+	witnessDictModify wit amap (MkWitnessDict cells) = fmap MkWitnessDict 
+		(replaceFirst ((fmap ((MkAny wit) . amap)) . (matchAny wit)) cells) where
 	{
 		replaceFirst :: (a -> Maybe a) -> [a] -> Maybe [a];
 		replaceFirst ama (a:aa) = case ama a of
@@ -38,16 +23,6 @@ module Data.Witness.WitnessDict
 			_ -> fmap (a :) (replaceFirst ama aa);
 		};
 		replaceFirst _ _ = Nothing;
-		
-		mapCell :: (Witness w) => w a -> (a -> a) -> Any w -> Maybe (Any w);
-		mapCell wit' amap' (MkAny w a') = mapAny (\cwit a -> do
-		{
-			MkEqualType <- matchWitness wit' cwit;
-			return (amap' a);
-		}) (MkAny w a');
-		
-		mapAny :: (forall a. w a -> a -> Maybe a) -> Any w -> Maybe (Any w);
-		mapAny wama (MkAny w a) = fmap (MkAny w) (wama w a);
 	};
 
 	witnessDictReplace :: (Witness w) => w a -> a -> WitnessDict w -> Maybe (WitnessDict w);
@@ -55,6 +30,16 @@ module Data.Witness.WitnessDict
 	
 	witnessDictAdd :: w a -> a -> WitnessDict w -> WitnessDict w;
 	witnessDictAdd wit a (MkWitnessDict cells) = MkWitnessDict ((MkAny wit a):cells);
+	
+	witnessDictRemove :: (Witness w) => w a -> WitnessDict w -> Maybe (WitnessDict w);
+	witnessDictRemove wit (MkWitnessDict cells) = fmap MkWitnessDict 
+		(removeFirst (\(MkAny cwit _) -> isJust (matchWitness wit cwit)) cells) where
+	{
+		removeFirst :: (a -> Bool) -> [a] -> Maybe [a];
+		removeFirst p (a:as) | p a = Just as;
+		removeFirst p (a:as) = fmap (a :) (removeFirst p as);
+		removeFirst _ _ = Nothing;
+	};
 	
 	witnessDictFromList :: (Witness w) => [Any w] -> WitnessDict w;
 	witnessDictFromList = MkWitnessDict;
