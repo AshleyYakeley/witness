969931cd925f7b9deb0967c53392e4fb0888698c
Ashley Yakeley <ashley@semantic.org>
2008-06-16 23:23:51 -0700

    don't use Maybe in modify & replace in WitnessDict
    darcs-hash:20080617062351-ac6dd-07379616a4a53a73e2d3820160cded47e5226f09

--- Data/Witness/WitnessDict.hs
+++ Data/Witness/WitnessDict.hs
@@ -12,33 +12,33 @@ module Data.Witness.WitnessDict where
 	witnessDictLookup :: (Witness w) => w a -> WitnessDict w -> Maybe a;
 	witnessDictLookup wit (MkWitnessDict cells) = listToMaybe (mapMaybe (matchAny wit) cells);
 	
-	witnessDictModify :: (Witness w) => w a -> (a -> a) -> WitnessDict w -> Maybe (WitnessDict w);
-	witnessDictModify wit amap (MkWitnessDict cells) = fmap MkWitnessDict 
+	witnessDictModify :: (Witness w) => w a -> (a -> a) -> WitnessDict w -> WitnessDict w;
+	witnessDictModify wit amap (MkWitnessDict cells) = MkWitnessDict 
 		(replaceFirst ((fmap ((MkAny wit) . amap)) . (matchAny wit)) cells) where
 	{
-		replaceFirst :: (a -> Maybe a) -> [a] -> Maybe [a];
+		replaceFirst :: (a -> Maybe a) -> [a] -> [a];
 		replaceFirst ama (a:aa) = case ama a of
 		{
-			Just newa -> Just (newa:aa);
-			_ -> fmap (a :) (replaceFirst ama aa);
+			Just newa -> (newa:aa);
+			_ -> a : (replaceFirst ama aa);
 		};
-		replaceFirst _ _ = Nothing;
+		replaceFirst _ _ = [];
 	};
 
-	witnessDictReplace :: (Witness w) => w a -> a -> WitnessDict w -> Maybe (WitnessDict w);
+	witnessDictReplace :: (Witness w) => w a -> a -> WitnessDict w -> WitnessDict w;
 	witnessDictReplace wit newa = witnessDictModify wit (const newa);
 	
 	witnessDictAdd :: w a -> a -> WitnessDict w -> WitnessDict w;
 	witnessDictAdd wit a (MkWitnessDict cells) = MkWitnessDict ((MkAny wit a):cells);
 	
-	witnessDictRemove :: (Witness w) => w a -> WitnessDict w -> Maybe (WitnessDict w);
-	witnessDictRemove wit (MkWitnessDict cells) = fmap MkWitnessDict 
+	witnessDictRemove :: (Witness w) => w a -> WitnessDict w -> WitnessDict w;
+	witnessDictRemove wit (MkWitnessDict cells) = MkWitnessDict 
 		(removeFirst (\(MkAny cwit _) -> isJust (matchWitness wit cwit)) cells) where
 	{
-		removeFirst :: (a -> Bool) -> [a] -> Maybe [a];
-		removeFirst p (a:as) | p a = Just as;
-		removeFirst p (a:as) = fmap (a :) (removeFirst p as);
-		removeFirst _ _ = Nothing;
+		removeFirst :: (a -> Bool) -> [a] -> [a];
+		removeFirst p (a:as) | p a = as;
+		removeFirst p (a:as) = a : (removeFirst p as);
+		removeFirst _ _ = [];
 	};
 	
 	witnessDictFromList :: (Witness w) => [Any w] -> WitnessDict w;
