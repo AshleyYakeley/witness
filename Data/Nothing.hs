49084349059d99777b81c2cbaa97030913457c84
Ashley Yakeley <ashley@semantic.org>
2010-05-26 00:08:47 -0700

    some instances for Nothing
    Ignore-this: 9fd547375172d076072a9a548052cf28
    
    darcs-hash:20100526070847-ac6dd-29b8df22ea0e31cf5393f92db24f34a3b0ef867a

--- Data/Nothing.hs
+++ Data/Nothing.hs
@@ -1,7 +1,22 @@
 module Data.Nothing where
 {
-	data Nothing;
-	
-	never :: Nothing -> a;
-	never n = seq n undefined;
+    data Nothing;
+
+    never :: Nothing -> a;
+    never n = seq n undefined;
+
+    instance Eq Nothing where
+    {
+        a == _b = never a;
+    };
+
+    instance Ord Nothing where
+    {
+        a <= _b = never a;
+    };
+
+    instance Show Nothing where
+    {
+        show a = never a;
+    };
 }
