176061893e4fb36658c31084b1112931bcf2924a
Ashley Yakeley <ashley@semantic.org>
2020-06-12 15:14:33 -0700

    anyToValue, valueToAny

--- src/Data/Witness/WitnessValue.hs
+++ src/Data/Witness/WitnessValue.hs
@@ -1,8 +1,21 @@
 module Data.Witness.WitnessValue where
 
 import Data.Kind
+import Data.Witness.Any
 
 class WitnessValue (w :: k -> Type) where
     type WitnessValueType w :: Type
     witnessToValue :: forall (t :: k). w t -> WitnessValueType w
     valueToWitness :: forall r. WitnessValueType w -> (forall (t :: k). w t -> r) -> r
+
+anyToValue ::
+       forall k (w :: k -> Type). WitnessValue w
+    => AnyW w
+    -> WitnessValueType w
+anyToValue (MkAnyW wt) = witnessToValue wt
+
+valueToAny ::
+       forall k (w :: k -> Type). WitnessValue w
+    => WitnessValueType w
+    -> AnyW w
+valueToAny v = valueToWitness v MkAnyW
