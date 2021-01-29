0f3a7a831b87740e7e0eeac1aa15b434de62bce8
Ashley Yakeley <ashley@semantic.org>
2021-01-28 23:17:40 -0800

    HetEqual type

--- src/Data/Type/Heterogeneous.hs
+++ src/Data/Type/Heterogeneous.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE StandaloneKindSignatures #-}
+
 module Data.Type.Heterogeneous where
 
 import Data.Kind
@@ -13,3 +15,11 @@ homoHetEq HRefl = Refl
 
 class TestHetEquality (w :: forall k. k -> Type) where
     testHetEquality :: forall (ka :: Type) (a :: ka) (kb :: Type) (b :: kb). w a -> w b -> Maybe (a :~~: b)
+
+-- | Equivalent to (:~~:), but can be made an instance of 'TestHetEquality'
+type HetEqual :: forall ka. ka -> forall kb. kb -> Type
+data HetEqual a b where
+    HetRefl :: forall k (a :: k). HetEqual a a
+
+instance forall k (a :: k). TestHetEquality (HetEqual a) where
+    testHetEquality HetRefl HetRefl = Just HRefl
